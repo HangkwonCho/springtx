b85f6ae502f781095376522c6c12740a94538afc
HangkwonCho <vatican0411@gmail.com>
2023-03-26 23:10:17 +0900

    트랜잭션 전파 활용1

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,89 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * MemberService    @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository    @Transactional:ON
+     */
+    @Test
+    void outerTxOff_success() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService    @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository    @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        // given
+        String username = "로그예외_outerTxOff_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+//        memberService.joinV1(username);
+
+        // then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:OFF
+     * LogRepository @Transactional:OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+        //when
+        memberService.joinV1(username);
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:OFF
+     * LogRepository @Transactional:OFF Exception
+     */
+    @Test
+    void singleTx_fail() {
+        //given
+        String username = "로그예외_singleTx";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+}
