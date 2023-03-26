b4d48366114adf45d77f7746ed2e9e9bf90d047a
HangkwonCho <vatican0411@gmail.com>
2023-03-26 23:31:02 +0900

    트랜잭션 전파 활용2

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -73,15 +73,35 @@ class MemberServiceTest {
 
     /**
      * MemberService @Transactional:ON
-     * MemberRepository @Transactional:OFF
-     * LogRepository @Transactional:OFF Exception
+     * MemberRepository @Transactional:OM
+     * LogRepository @Transactional:ON
      */
     @Test
-    void singleTx_fail() {
+    void outerTxOn_success() {
         //given
-        String username = "로그예외_singleTx";
+        String username = "outerTxOn_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:OM
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOn_fail";
+
         //when
         assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
         //then: 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isEmpty());
         assertTrue(logRepository.find(username).isEmpty());
